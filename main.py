97348195ae1685c78166d1736feffc7f669d20a2
yangsheng <wu0306109@gmail.com>
2023-02-15 14:14:58 +0800

    Add `create_app()`

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 import os
 
-from flask import Flask
+from drchen import create_app
 
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+app = create_app()
 
 if __name__ == '__main__':
     app.run(
